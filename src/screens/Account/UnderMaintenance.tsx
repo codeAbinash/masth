09b3c42b66f07254432efb7289bc31f157da1196
Abinash Karmakar <codeAbinash@gmail.com>
2024-04-14 02:03:01 +0530

    Close application after clicking close app button in UnderMaintenance.tsx

--- src/screens/Account/UnderMaintenance.tsx
+++ src/screens/Account/UnderMaintenance.tsx
@@ -1,7 +1,7 @@
 import { SmallButton } from '@components/Button'
 import PhoneSettingIcon from '@icons/phone_setting.svg'
 import React from 'react'
-import { Dimensions, Text, View } from 'react-native'
+import { BackHandler, Dimensions, Text, View } from 'react-native'
 
 const { width, height } = Dimensions.get('window')
 
@@ -15,7 +15,13 @@ export default function UnderMaintenance() {
         <Text className='text-3xl font-bold'>System Under maintenance</Text>
         <Text className='px-2 text-center text-lg'>Please be patient we will make the app live soon.</Text>
         <View className='pt-5'>
-          <SmallButton title='Close App' className='px-10 py-3' />
+          <SmallButton
+            title='Close App'
+            className='px-10 py-3'
+            onPress={() => {
+              BackHandler.exitApp()
+            }}
+          />
         </View>
       </View>
     </View>
